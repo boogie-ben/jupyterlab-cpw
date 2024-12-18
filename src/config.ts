c258153f8323becc284c791e1e76c928f6e9fc98
boogie-ben <Boogie.Ben@outlook.com>
2024-12-19 02:13:31 +0800

    fix: dev判断

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-// export const isDev = import.meta.env.MODE === 'dev'
-export const isDev = window.location.host !== 'lab.devops.tencent-cep.com'
+export const isDev = import.meta.env.MODE === 'dev'
+// export const isDev = window.location.host !== 'lab.devops.tencent-cep.com'
